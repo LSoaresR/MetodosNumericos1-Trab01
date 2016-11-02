1d435101421cca5874e7dc8c6681167b2fc70090
Victor de Almeida <victoralmeida.main@gmail.com>
2016-11-02 16:02:43 -0300

    refactor Dados

--- src/Dados.cpp
+++ src/Dados.cpp
@@ -6,11 +6,12 @@ using namespace std;
 Dados::Dados(){	
 }
 
-Dados::Dados(vector<double> x, vector<double> phi, vector<double> fx, vector<double> fx_der) {
+Dados::Dados(vector<double> x, vector<double> phi, vector<double> fx, vector<double> fx_der, double lambda) {
 	this->x.swap(x);
 	this->phi.swap(phi);
 	this->fx.swap(fx);
 	this->fx_der.swap(fx_der);
+	this->lambda;
 }
 
 //SETS
@@ -30,6 +31,10 @@ void Dados::setFx_der(double fx_der) {
 	this->fx_der.push_back(fx_der);
 }
 
+void Dados::setLambda(double lambda){
+	this->lambda = lambda;
+}
+
 //GETS 
 vector<double> Dados::getX() {
 	return x;
@@ -45,4 +50,8 @@ vector<double> Dados::getFx() {
 
 vector<double> Dados::getFx_der() {
 	return fx_der;
+}
+
+double Dados::getLambda(){
+	return lambda;
 }
--- src/Dados.h
+++ src/Dados.h
@@ -8,18 +8,20 @@ class Dados {
 	public:
 		//construtor
 		Dados();
-		Dados(vector<double> x, vector<double> phi, vector<double> fx, vector<double> fx_der);
+		Dados(vector<double> x, vector<double> phi, vector<double> fx, vector<double> fx_der, double lambda);
 	
 		//sets
 		void setX(double x);
 		void setPhi(double phi);
 		void setFx(double fx);
-		void setFx_der(double fx_der);	
+		void setFx_der(double fx_der);
+		void setLambda(double lambda);
 		//gets
 		vector<double> getX();
 		vector<double> getPhi();
 		vector<double> getFx();
 		vector<double> getFx_der();
+		double getLambda();
 		
 	private:
 		
@@ -27,6 +29,7 @@ class Dados {
 		vector<double> phi;
 		vector<double> fx;
 		vector<double> fx_der;
+		double lambda;
 	
 	
 	
