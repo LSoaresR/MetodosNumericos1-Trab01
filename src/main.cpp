9ac6b1a044b1497cd4971de1cc955ef3ef12c937
Victor de Almeida <victoralmeida.main@gmail.com>
2016-11-07 09:35:28 -0300

    refactor NewtonRaphson

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "newthon-raphson.cpp"
+#include "NewtonRaphson.cpp"
 #include "Dados.h"
 #include <iostream>
 
@@ -8,13 +8,13 @@ using namespace std;
 
 int main() {
 	int n = 1;
-	NewthonRaphson r;
+	NewtonRaphson r;
   //	r.calibrarSistema(1, 1, 0.05, 1000, 0.05);
 	
   ///	r.quadroRespostaNRO(3, 2, -2.5, 0.001, 0.001, 1000);
 	cout << "Entre com  a quantidade de lambdas" << endl;
 	cin >> n;
-	r.NquadroRespostaNRFL(3, 2, 2.5, 0.001, 0.001, 1000, n);
+	r.quadroRespostaNRFLN(3, 2, 2.5, 0.001, 0.001, 1000, n);
 //	r.quadroRespostaNRFL(1, 1, 2.5, 0.05, 0.05, 1000, 0.05);
   //	r.quadroRespostaNRFL(1, 1, 5, 0.0001, 0.0001, 1000, 0.0001);
 
